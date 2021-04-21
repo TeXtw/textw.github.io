3251dd8d47fc16b69cbf101f3a665d509f7cd99e
Hsin-Hsiang Peng <hsinspeng@gmail.com>
2021-04-21 23:55:51 +0800

    Update docusaurus.config.js

--- docusaurus.config.js
+++ docusaurus.config.js
@@ -13,6 +13,15 @@ module.exports = {
   onBrokenMarkdownLinks: 'warn',
   organizationName: 'textw', // Usually your GitHub org/user name.
   projectName: 'textw.github.io', // Usually your repo name.
+  stylesheets: [
+    {
+      href: 'https://cdn.jsdelivr.net/npm/katex@0.12.0/dist/katex.min.css',
+      type: 'text/css',
+      integrity:
+        'sha384-AfEj0r4/OFrOo5t7NnNe46zW/tFgW6x/bCJG8FqQCEo3+Aro6EYUG4+cU+KJWu/X',
+      crossorigin: 'anonymous',
+    },
+  ],
   themeConfig: {
     navbar: {
       title: 'TWTUG',
